f0ca6a6acc5021c4b9dec8a9c118861157447d3c
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-01-31 10:36:44 +0100

    :wrench: Installed multer

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const multer = require('multer'); 
+
+let storage = multer.diskStorage({
+    destination: function(req, res, cb) {
+        cb(null, "public/upload"); 
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ storage: storage }); 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,5 +28,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.get("/image", (req, res) => {
+    let upForm = `
+    <form action="/savefile" method="post" enctype="multipart/form-data">
+        <input type="file" name="image">
+        <button type="submit">Skicka</button> 
+    </form>
+    `
+    res.send(upForm)
+})
+
+app.post("/savefile", upload.single("image"), (req, res) => {
+    console.log(req.file);
+    res.send(`<img src="/upload/${req.file.filename}">`)
+})
 
 module.exports = app;
